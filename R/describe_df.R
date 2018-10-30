354736fde960c693ebb8684b800deac964c32c0f
EJ <johe@smh.ca>
2018-10-30 10:42:02 -0400

    Remove redundant script
    removed vector output for spss_col_labels script

--- R/describe_df.R
+++ R/describe_df.R
@@ -1,5 +1,5 @@
 #' Quantifies missing values found within a data frame
-#' 
+#'
 #' This \code{map} wrapper function vectorizes the count and proportion of missing and non-missing values within
 #' a tidy data frame.
 #'
@@ -8,28 +8,28 @@
 #'
 #' @return Returns a \code{data frame} with four columns, each row displays the number and percentage of missing
 #' values, and the percentage of existing (non-missing) values for each variable within the data frame
-#' 
+#'
 #' @import purrr, tidyr, dplyr
-#' 
+#'
 #'
 #' @examples
 #' missing_df(airquality)
-#' 
+#'
 #' @export
-#' 
+#'
 missing_df <- function(df, vars = names(df)) { #identifies missing values in columns of df
-  
-  df[,vars] %>%
-    map_dfc(~sum(is.na(.))) %>% 
-    gather(key = col_name, value = n_missing) %>% 
-    mutate(percent_missing = round(100*n_missing/nrow(df),2),
-           percent_exist = 100-percent_missing)
+
+  df[, vars] %>%
+    map_dfc(~sum(is.na(.))) %>%
+    gather(key = col_name, value = n_missing) %>%
+    mutate(percent_missing = round(100 * n_missing / nrow(df), 2),
+           percent_exist = 100 - percent_missing)
   }
 
 
 #' Identifies class for each variable in data frame
-#' 
-#' This \code{map} wrapper function vectorizes the variable data type values within a tidy data frame. 
+#'
+#' This \code{map} wrapper function vectorizes the variable data type values within a tidy data frame.
 #' Maps the \code{\link{class}} function for all select variables of a data frame.
 #'
 #' @param df a \code{data frame} object to identify the column class from select variables of a data frame
@@ -37,37 +37,37 @@ missing_df <- function(df, vars = names(df)) { #identifies missing values in col
 #'
 #' @return Returns a \code{data frame} with two columns where each row is a variable from the data frame of interest
 #' and the data type ()
-#' 
+#'
 #' @import purrr, tidyr, dplyr
-#' 
+#'
 #' @export
 #'
 #' @examples
 #' type_df(iris)
-#' 
+#'
 type_df <- function(df, vars = names(df)) { #identifies the class of columns in df
 
-  df[,vars] %>% 
+  df[, vars] %>%
     map_dfc(~class(.)) %>%
     gather(key = col_name, value = data_type)
   }
 
 
 #' Identifies foreign labels within a data frame
-#' 
-#' This function identifies labels associated with foreign data types such as Stata or SPSS. 
+#'
+#' This function identifies labels associated with foreign data types such as Stata or SPSS.
 #' Typically object types are imported using the \code{\link{haven}} package.
 #'
 #' @param df a \code{data frame} object to identify the foreign labels from select variables of a data frame
 #' @param vars a \code{character string} or \code{character vector} of column names in the data frame to examine
 #'
-#' @return returns a \code{data frame} with a \code{logical}  column containing whether the specified 
+#' @return returns a \code{data frame} with a \code{logical}  column containing whether the specified
 #' columns have a foreign label associated it.
-#' 
+#'
 #' @import purrr, tidyr, dplyr, haven
-#' 
+#'
 #' @export
-#' 
+#'
 #'
 #' @examples
 #' type_df(airquality)
@@ -75,81 +75,81 @@ type_df <- function(df, vars = names(df)) { #identifies the class of columns in 
 lab_foreign_df <- function(df, vars = names(df)) { #identifies foreign labels (imported) in columns of df
   require(purrr)
   require(tidyr)
-  
-  df[,vars] %>% 
-    map_dfc(~is.labelled(.)) %>% 
+
+  df[,vars] %>%
+    map_dfc(~is.labelled(.)) %>%
     gather(key = col_name, value = label)
   }
 
 #' Summarizes Missing Data, Data Types, and Foreign Labels into a tidy data frame
-#' 
+#'
 #' This function uses \code{type_df}, \code{missing_df}, \code{lab_foreign_df} to provide information on the
-#' variables contained within the \code{df} input. 
+#' variables contained within the \code{df} input.
 #'
 #' @param df a \code{data frame} object to identify the foreign labels from select variables of a data frame
 #' @param vars a \code{character string} or \code{character vector} of column names in the data frame to examine
 #'
-#' @return returns a \code{data frame} where column from \code{df} is a row and 
+#' @return returns a \code{data frame} where column from \code{df} is a row and
 #' information on the number of missing values, data type, and foreign labels are specified.
-#' 
+#'
 #' @import purrr, tidyr, dplyr, haven
-#' 
+#'
 #' @export
-#' 
+#'
 #' @examples
 #' describe_df(airquality)
-#' 
+#'
 describe_df <- function(df, vars = names(df), ...) {
   require(purrr)
   require(dplyr)
-  
+
   # miss <- missing_df(df,...)
   # type <- type_df(df,...)
   # f_lab <- lab_foreign_df(df,...)
-  
-  if(nrow(missing_df(df,vars)) != nrow(type_df(df,vars)) | #logical check for different column sizes
-     nrow(type_df(df,vars)) != nrow(lab_foreign_df(df,vars)) |
-     nrow(missing_df(df,vars)) != nrow(lab_foreign_df(df,vars))) {
+
+  if(nrow(missing_df(df, vars)) != nrow(type_df(df, vars)) | #logical check for different column sizes
+     nrow(type_df(df, vars)) != nrow(lab_foreign_df(df, vars)) |
+     nrow(missing_df(df, vars)) != nrow(lab_foreign_df(df, vars))) {
     stop("Number of original columns do not match, specific 'vars' in function parameters")
-    
+
     } else {
-    df1 <- missing_df(df,vars) %>% left_join(type_df(df,vars), by = "col_name") %>% 
-      left_join(lab_foreign_df(df,vars), by = "col_name") %>% 
-      select(col_name,everything())
-    
+    df1 <- missing_df(df,vars) %>% left_join(type_df(df,vars), by = "col_name") %>%
+      left_join(lab_foreign_df(df, vars), by = "col_name") %>%
+      select(col_name, everything())
+
     }
-  
+
   return(df1)
-  
+
 }
 
 #' Plots Missing Data, Data Types, and Foreign Labels into a tidy data frame
-#' 
-#' This function takes the \code{describe_df} function, built on 
+#'
+#' This function takes the \code{describe_df} function, built on
 #' \code{type_df}, \code{missing_df}, \code{lab_foreign_df} to plot information on the
-#' variables contained within the \code{df} input. 
+#' variables contained within the \code{df} input.
 #'
 #' @param df a \code{data frame} object to identify the foreign labels from select variables of a data frame
 #' @param vars a \code{character string} or \code{character vector} of column names in the data frame to examine
 #'
 #' @return returns a \code{ggplot2} plot displaying the percentage of missing values and data types
 #' for each column contained in the data frame
-#' 
+#'
 #' @import purrr, tidyr, dplyr, haven
-#' 
+#'
 #' @export
-#' 
+#'
 #' @examples
 #' describe_df(airquality)
 #' describe_viz(airquality)
-#' 
+#'
 describe_viz <- function(df, title = NULL,subtitle = NULL, threshold = 10, ...) { #visualizes the summary of above functions
   require(ggplot2)
   require(forcats)
-  
-  df1 <- describe_df(df, ...) 
+
+  df1 <- describe_df(df, ...)
   order_name <- df1 %>% pull(col_name) #ordering names of columns (preserve original order)
-  
+
   df1 <- df1 %>% mutate(col_name_f=factor(col_name,levels = rev(order_name)))
 
   df1 %>%
@@ -159,6 +159,5 @@ describe_viz <- function(df, title = NULL,subtitle = NULL, threshold = 10, ...) 
     labs(title = title, subtitle = subtitle,
          x = "Column Name", y = "Percentage Missing (%)",...) +
     scale_fill_brewer(type = "qual", palette = 5)
-  
-}
 
+}
--- R/lsos.R
+++ R/lsos.R
@@ -1,21 +1,21 @@
 #' List Objects in Global Environment
-#' 
+#'
 #' This \code{lsos} function summarizes all objects found in the global environment into a \code{data frame}.
-#' If using \code{RStudio}, this information can be viewed using the "Environment" Tab in the GUI. 
+#' If using \code{RStudio}, this information can be viewed using the "Environment" Tab in the GUI.
 #'
 #' See this Stack Overflow post: https://stackoverflow.com/questions/1358003/tricks-to-manage-the-available-memory-in-an-r-session
 #'
 #' @examples
 #' data(iris)
 #' data(airquality)
-#' 
+#'
 #' x <- letters[1:15]
 #' y <- matrix(data = rnorm(n = 50),ncol = 10)
-#' 
+#'
 #' lsos(n = 50)
-#' 
+#'
 #' @export
-#' 
+#'
 .ls.objects <- function (pos = 1, pattern, order.by,
                          decreasing=FALSE, head=FALSE, n=5) {
   napply <- function(names, fn) sapply(names, function(x)
@@ -34,12 +34,12 @@
   out <- data.frame(obj.type, obj.size, obj.prettysize, obj.dim)
   names(out) <- c("Type", "Size", "PrettySize", "Length/Rows", "Columns")
   if (!missing(order.by))
-    out <- out[order(out[[order.by]], decreasing=decreasing), ]
+    out <- out[order(out[[order.by]], decreasing = decreasing), ]
   if (head)
     out <- head(out, n)
   out
 }
 # shorthand
-lsos <- function(..., n=10) {
-  .ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
+lsos <- function(..., n = 10) {
+  .ls.objects(..., order.by = "Size", decreasing = TRUE, head = TRUE, n = n)
 }

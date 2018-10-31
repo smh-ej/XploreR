5fb686112bc6d08c36c5367a1166239e88b09029
EJ <johe@smh.ca>
2018-10-31 12:02:12 -0400

    Revert "added object doc"
    This reverts commit dd4e5ec13e1e2cc53fb77ca6da6f176864a4ccf1.

--- R/check_updated_packs.R
+++ R/check_updated_packs.R
@@ -1,22 +1,3 @@
-#' Convenience function for checking package versions
-#'
-#' This function provides a simple way to check the versions of installed
-#' packages by utilizing the \code{old.packages} function from the \code{utils}
-#' package.
-#' 
-#' See \code{\link{old.packages}}
-#'
-#' @importFrom utils old.packages
-#'
-#' @return a \code{data frame} object of all R pacakges in \code{LibPath} that 
-#' have differing versions between what is installed and the CRAN Repo.
-#' 
-#' @export
-#'
-#' @examples
-#' check_updated_packs()
-#' 
-#' 
 check_updated_packs <- function(x) {
   
   old_df <- as.data.frame(old.packages())
